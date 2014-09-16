ae42fa94aa8ee7b3a3b07e4f0391f15a0f2625bb
Henryk Konsek <hekonsek@gmail.com>
2014-09-16 22:35:10 +0200

    Added main jar support.

--- startup.sh
+++ startup.sh
@@ -9,5 +9,11 @@ echo
 # TODO allow this to be disabled via an env var
 #service sshd start
 
-echo "executing java $FABRIC8_JAVA_AGENT $FABRIC8_JVM_ARGS -cp "classes:lib/*" $FABRIC8_JAVA_MAIN $FABRIC8_MAIN_ARGS"
-java $FABRIC8_JAVA_AGENT $FABRIC8_JVM_ARGS -cp "classes:lib/*" $FABRIC8_JAVA_MAIN $FABRIC8_MAIN_ARGS
+if [ -z "$FABRIC8_JAR" ]; then
+  echo "executing java $FABRIC8_JAVA_AGENT $FABRIC8_JVM_ARGS -cp "classes:lib/*" $FABRIC8_JAVA_MAIN $FABRIC8_MAIN_ARGS"
+  java $FABRIC8_JAVA_AGENT $FABRIC8_JVM_ARGS -cp "classes:lib/*" $FABRIC8_JAVA_MAIN $FABRIC8_MAIN_ARGS
+else
+  echo "executing java $FABRIC8_JAVA_AGENT $FABRIC8_JVM_ARGS -jar $FABRIC8_JAR $FABRIC8_MAIN_ARGS"
+  java $FABRIC8_JAVA_AGENT $FABRIC8_JVM_ARGS -jar $FABRIC8_JAR $FABRIC8_MAIN_ARGS
+fi
+
